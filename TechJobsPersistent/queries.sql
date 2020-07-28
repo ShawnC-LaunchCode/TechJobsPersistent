ebe95e97f4c476797f2461dace743d7ca89da278
Shawn C <scooter4356@gmail.com>
2020-07-28 17:21:02 -0500

    finished, i think

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -5,6 +5,13 @@
 
 
 --Part 2
-
+SELECT Name 
+FROM employers 
+WHERE Location = "St. Louis";
 --Part 3
-
+SELECT *
+FROM Skills
+JOIN JobSkills
+ON Skills.Id = JobSkills.SkillId
+WHERE JobSkills.JobId IS NOT NULL
+ORDER BY Name ASC;
